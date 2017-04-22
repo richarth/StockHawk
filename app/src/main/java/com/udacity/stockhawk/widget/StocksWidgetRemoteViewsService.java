f3c5ff1adafff8a8b11237a5c35cd7eff8f3fe81
Richard Thompson <richarth@gmail.com>
2017-04-22 12:24:13 +0100

    Implemented an initial version of the widget

---
+++ app/src/main/java/com/udacity/stockhawk/widget/StocksWidgetRemoteViewsService.java
@@ -0,0 +1,153 @@
+package com.udacity.stockhawk.widget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Binder;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract;
+import com.udacity.stockhawk.data.PrefUtils;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import static com.udacity.stockhawk.data.Contract.Quote.POSITION_ABSOLUTE_CHANGE;
+import static com.udacity.stockhawk.data.Contract.Quote.POSITION_ID;
+import static com.udacity.stockhawk.data.Contract.Quote.POSITION_PERCENTAGE_CHANGE;
+import static com.udacity.stockhawk.data.Contract.Quote.POSITION_PRICE;
+import static com.udacity.stockhawk.data.Contract.Quote.POSITION_SYMBOL;
+import static com.udacity.stockhawk.data.Contract.Quote.QUOTE_COLUMNS;
+
+/**
+ * Created by richardthompson on 22/04/2017.
+ */
+
+public class StocksWidgetRemoteViewsService extends RemoteViewsService {
+    public final String LOG_TAG = StocksWidgetRemoteViewsService.class.getSimpleName();
+
+    @Override
+    public RemoteViewsService.RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RemoteViewsService.RemoteViewsFactory() {
+            private Cursor data = null;
+
+            @Override
+            public void onCreate() {
+                // Nothing to do
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data != null) {
+                    data.close();
+                }
+                // This method is called by the app hosting the widget (e.g., the launcher)
+                // However, our ContentProvider is not exported so it doesn't have access to the
+                // data. Therefore we need to clear (and finally restore) the calling identity so
+                // that calls use our process and permission
+                final long identityToken = Binder.clearCallingIdentity();
+                Uri stocksUri = Contract.Quote.URI;
+                data = getContentResolver().query(stocksUri,
+                        QUOTE_COLUMNS.toArray(new String[]{}),
+                        null,
+                        null,
+                        QUOTE_COLUMNS.get(POSITION_ID) + " ASC");
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION ||
+                        data == null || !data.moveToPosition(position)) {
+                    return null;
+                }
+                RemoteViews views = new RemoteViews(getPackageName(),
+                        R.layout.list_item_quote);
+
+                DecimalFormat dollarFormat = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+                DecimalFormat dollarFormatWithPlus = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+                dollarFormatWithPlus.setPositivePrefix("+$");
+                DecimalFormat percentageFormat = (DecimalFormat) NumberFormat.getPercentInstance(Locale.getDefault());
+                percentageFormat.setMaximumFractionDigits(2);
+                percentageFormat.setMinimumFractionDigits(2);
+                percentageFormat.setPositivePrefix("+");
+
+                String stockSymbol = data.getString(POSITION_SYMBOL);
+                double stockPrice = data.getFloat(POSITION_PRICE);
+
+                String stockPriceFormatted = dollarFormat.format(stockPrice);
+
+                views.setTextViewText(R.id.symbol, stockSymbol);
+                views.setTextViewText(R.id.price, stockPriceFormatted);
+
+                float rawAbsoluteChange = data.getFloat(POSITION_ABSOLUTE_CHANGE);
+                float percentageChange = data.getFloat(POSITION_PERCENTAGE_CHANGE);
+
+                if (rawAbsoluteChange > 0) {
+                    views.setInt(R.id.change, "setBackgroundResource", R.drawable.percent_change_pill_green);
+                } else {
+                    views.setInt(R.id.change, "setBackgroundResource", R.drawable.percent_change_pill_red);
+                }
+
+                String change = dollarFormatWithPlus.format(rawAbsoluteChange);
+                String percentage = percentageFormat.format(percentageChange / 100);
+
+                Context context = StocksWidgetRemoteViewsService.this;
+
+                if (PrefUtils.getDisplayMode(context)
+                        .equals(context.getString(R.string.pref_display_mode_absolute_key))) {
+                    views.setTextViewText(R.id.change, change);
+                } else {
+                    views.setTextViewText(R.id.change, percentage);
+                }
+
+                final Intent fillInIntent = new Intent();
+                Uri stockUri = Contract.Quote.makeUriForStock(stockSymbol);
+                fillInIntent.setData(stockUri);
+                views.setOnClickFillInIntent(R.id.list_item_quote, fillInIntent);
+
+                return views;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.list_item_quote);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (data.moveToPosition(position))
+                    return data.getLong(POSITION_ID);
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+}
