adfbd9dbdb74dade9345ce04357922b1ad3d6bd5
Richard Thompson <richarth@gmail.com>
2017-05-09 18:38:03 +0100

    Added missing space

--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -172,7 +172,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             error.setVisibility(View.GONE);
         }
 
-        if (!data.isClosed()){
+        if (!data.isClosed()) {
             adapter.setCursor(data);
         }
     }
